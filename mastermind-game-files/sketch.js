7458c54e3b645d68fceb7a537e04ccb2aae44d27
chodges7 <chodges7@mail.csuchico.edu>
2022-02-14 14:15:03 -0800

    feat: change object oriented squares to be drawn every frame

--- mastermind-game-files/sketch.js
+++ mastermind-game-files/sketch.js
@@ -1,40 +1,56 @@
-function setup() {
-  let width = 450;
-  let height = 600;
-  let columns = 4;
-  let rows = 6;
-  createCanvas(width, height);
-  background(40);
+let w = 450;          // width of the canvas
+let h = 600;          // height of the canvas
+let columns = 4;      // columns in the game (how long the words will be)
+let rows = 6;         // rows in the game (total amt of guesses)
+let gridSize = 75;    // size of the grid boxes
+let squareSize = 60;  // size of the letter boxes 
+let yOffset = 45;     // offset for pushing the grid up/down
+let xOffset = 0;      // offset for pushing the grid left/right
+let yMargin = (h - rows * gridSize) / 2 + yOffset; // calculating the xmargin
+let xMargin = (w - columns * gridSize) / 2 + xOffset; // calculating the ymargin
 
-  const letters = new Array();
-  for (let i = 0; i < columns; i++) {
-    letters[i] = new Array();
-  }
-
-  const gridBoxes = new Array();
-  for (let i = 0; i < columns; i++) {
-    gridBoxes[i] = new Array();
-  }
-
-  let gridSize = 75;
-  let squareSize = 60;
-  let yOffset = 45;
-  let xOffset = 0;
-  let yMargin = (height - rows * gridSize) / 2 + yOffset;
-  let xMargin = (width - columns * gridSize) / 2 + xOffset;
-
-  for (let i = 0; i < columns; i++) {
-    for (let j = 0; j < rows; j++) {
-      let xPos = i * gridSize + xMargin + (gridSize - squareSize) / 2;
-      let yPos = j * gridSize + yMargin + (gridSize - squareSize) / 2;
-
-      let gridxPos = i * gridSize + xMargin;
-      let gridyPos = j * gridSize + yMargin;
-
-      gridBoxes[i][j] = new LetterBox(gridxPos, gridyPos, gridSize, 4);
-      letters[i][j] = new LetterBox(xPos, yPos, squareSize, 10);
-    }
+// variables for tracking the letters
+let xCur = 0;
+let yCur = 0;
+const letters = new Array();
+for (let i = 0; i < columns; i++) {
+  letters[i] = new Array;
+  for (let j = 0; j < rows; j++) {
+    letters[i][j] = null
   }
 }
 
-function draw() {}
+function setup() {
+  // Create the canvas we'll work on as well as the background color
+  createCanvas(w, h);
+  background(40);
+} // setup()
+
+function draw() {
+  // TODO: check the keyboard for input. 
+  //   When a letter is input update the letters array at that position.
+
+  /* ----- Draw the grid and letter boxes ----- */
+  // for each box in the grid...
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      // ...set the xPos and yPos for the letters
+      let xPos = i * gridSize + xMargin + (gridSize - squareSize) / 2;
+      let yPos = j * gridSize + yMargin + (gridSize - squareSize) / 2;
+
+      // ...set the gridxPos and gridyPos for this gridBoxes
+      let gridxPos = i * gridSize + xMargin;
+      let gridyPos = j * gridSize + yMargin;
+
+      // ...create the two boxes in the grid
+      square(gridxPos, gridyPos, gridSize, 4);
+      square(xPos, yPos, squareSize, 10);
+
+      // ...and finally, 
+      if (letters[i][j] != null) {
+        print("This letter is " + letters[i][j])
+      }
+    }
+  }
+
+} // draw()
