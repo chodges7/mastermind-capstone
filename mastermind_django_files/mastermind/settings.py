acd21732e8ca99aea34fcdaca9a1e7eeec229a2b
chodges7 <cdhodges5@gmail.com>
2022-03-05 17:52:49 -0800

    fix: add IP to allowed hosts

--- mastermind_django_files/mastermind/settings.py
+++ mastermind_django_files/mastermind/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.177.155.198",]
 
 
 # Application definition
