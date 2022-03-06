bd0343d0e39a48220e1ed469957d3f3264d62b9e
chodges7 <cdhodges5@gmail.com>
2022-03-05 18:00:46 -0800

    fix: wrong AWS_SES_REGION_NAME

--- mastermind_django_files/accounts/views.py
+++ mastermind_django_files/accounts/views.py
@@ -39,17 +39,21 @@ def password_reset_request(request):
 					email_template_name = "main/password/password_reset_email.txt"
 					c = {
                         "email": user.email,
-                        'site_name': 'Mastermind',
+                        'domain': 'your-website-name.com',
+                        'site_name': 'Website Name',
                         "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                        "user": user,
                         'token': default_token_generator.make_token(user),
-                        'protocol': 'http',
+                        'protocol': 'https',
 					}
 					email = render_to_string(email_template_name, c)
 					try:
 						send_mail(subject, email, 'cdhodges5@gmail.com', [user.email], fail_silently=False)
 					except BadHeaderError:
 						return HttpResponse('Invalid header found.')
-					return redirect("/password_reset/done/")
+
+					messages.success(
+					    request, 'A message with reset password instructions has been sent to your inbox.')
+					return redirect("main:homepage")
+			messages.error(request, 'An invalid email has been entered.')
 	password_reset_form = PasswordResetForm()
 	return render(request=request, template_name="main/password/password_reset.html", context={"password_reset_form": password_reset_form})
