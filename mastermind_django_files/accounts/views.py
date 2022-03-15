8fb2bf0b4d9ee1dc26ffb29d7bd8960f987d7a50
chodges7 <cdhodges5@gmail.com>
2022-03-15 12:22:02 -0700

    feat: add ajax to check if username exists on signup page

--- mastermind_django_files/accounts/views.py
+++ mastermind_django_files/accounts/views.py
@@ -1,6 +1,8 @@
 # accounts/views.py
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from . import forms
 
@@ -16,3 +18,10 @@ def signup(request):
         f = forms.CustomUserCreationForm()
 
     return render(request, 'registration/signup.html', {'form': f})
+
+def validate_username(request):
+    username = request.GET.get('username', None)
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(data)
