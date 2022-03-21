328ee2411f3964fe47581e0c07a38e8b6d72e4f1
chodges7 <cdhodges5@gmail.com>
2022-03-20 23:14:14 -0700

    fix: ajax and jquery weren't in correct order to work chore:  moved the username comparison to inside views.py

--- mastermind_django_files/accounts/views.py
+++ mastermind_django_files/accounts/views.py
@@ -30,4 +30,6 @@ def validate_username(request):
     data = {
         'is_taken': User.objects.filter(username__iexact=username).exists()
     }
+    if data['is_taken']:
+        data['error_message'] = 'A user with this username already exists.'
     return JsonResponse(data)
