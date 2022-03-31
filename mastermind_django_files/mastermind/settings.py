05536ff85f3a4b0e08a4837869fcd64827cc6124
chodges7 <cdhodges5@gmail.com>
2022-03-31 12:19:49 -0700

    chore: linter errors

--- mastermind_django_files/mastermind/settings.py
+++ mastermind_django_files/mastermind/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('POSTGRES_NAME'),
         'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),        
+        'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': '5432',             
+        'PORT': '5432',
     }
 }
 
