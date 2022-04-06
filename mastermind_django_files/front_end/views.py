71bfe1c120bd661fddd580f16a9c4e4935053881
chodges7 <cdhodges5@gmail.com>
2022-04-05 23:08:28 -0700

    fix: grab words better

--- mastermind_django_files/front_end/views.py
+++ mastermind_django_files/front_end/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
+import imp
 from django.http import HttpResponse
 from django.template import loader
 from random_word import RandomWords
+from random import choice
 # from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -14,12 +16,20 @@ def home_view(request):
 
 def game_view(request):
     template = loader.get_template('game.html')
-    r = RandomWords()
 
-    word = r.get_random_word(minLength=4, maxLength=4,
-                         hasDictionaryDef=True, minCorpusCount=50)
+    word = str(getWord())
+
     context = {
         'page_title':'Mastermind',
         'word':word
     }
     return HttpResponse(template.render(context, request))
+
+def getWord():
+    r = RandomWords()
+    list = r.get_random_words(minLength=4, maxLength=4, limit=10,
+                       hasDictionaryDef=True, minCorpusCount=50)
+    for word in list:
+        if '.' in word:
+            list.remove(word)
+    return choice(list)
