3956a9eda4f5a451edf152a8efbf3414cf6dda1f
chodges7 <cdhodges5@gmail.com>
2022-04-13 20:30:13 -0700

    feat: add games model

--- mastermind_django_files/front_end/models.py
+++ mastermind_django_files/front_end/models.py
@@ -1,3 +1,21 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+def default_json():
+    return {"guess0":"", "guess1":"", "guess2":"", "guess3":"", "guess4":"", "guess5":""}
+
+class Games(models.Model):
+
+    # Fields
+    game_id   = models.CharField(max_length = 50, primary_key = True)
+    completed = models.BooleanField(default = False)
+    guesses   = models.JSONField(default = default_json)
+    gamer     = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+
+    # Metadata
+    class Meta:
+        ordering = ['gamer', 'game_id']
+
+    # Methods
+    def __str__(self):
+        return self.game_id
