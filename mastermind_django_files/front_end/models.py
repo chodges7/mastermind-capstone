df91fd6adea36b505080f74576d3422a5ee5c276
chodges7 <cdhodges5@gmail.com>
2022-04-20 18:12:04 -0700

    chore: change default guesses JSON

--- mastermind_django_files/front_end/models.py
+++ mastermind_django_files/front_end/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 def default_json():
-    return {"guess0":"", "guess1":"", "guess2":"", "guess3":"", "guess4":"", "guess5":""}
+    return {"guesses": []}
 
 class Games(models.Model):
 
