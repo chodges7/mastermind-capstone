182449a3fd986c289fd73b76afd25a1a74cd5399
chodges7 <cdhodges5@gmail.com>
2022-04-24 17:56:05 -0700

    feat: add stats model

--- mastermind_django_files/front_end/models.py
+++ mastermind_django_files/front_end/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -5,12 +6,11 @@ def default_json():
     return {"guesses": ["default"]}
 
 class Games(models.Model):
-
     # Fields
-    game_id   = models.CharField(max_length = 50, primary_key = True)
-    completed = models.BooleanField(default = False)
-    guesses   = models.JSONField(default = default_json)
-    gamer     = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    game_id   = models.CharField(max_length=50, primary_key=True)
+    completed = models.BooleanField(default=False)
+    guesses   = models.JSONField(default=default_json)
+    gamer     = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     # Metadata
     class Meta:
@@ -21,3 +21,22 @@ class Games(models.Model):
     # Methods
     def __str__(self):
         return str(self.gamer) + " - " + self.game_id
+
+class Stats(models.Model):
+    # Fields
+    total_games   = models.IntegerField(default=0)
+    gamer         = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    total_guesses = models.IntegerField(default=0)
+    avg_guesses   = models.DecimalField(max_digits=4, decimal_places=3, default=0.000)
+    total_time    = models.DecimalField(max_digits=7, decimal_places=3, default=0000.000)
+    avg_time      = models.DecimalField(max_digits=7, decimal_places=3, default=0000.000)
+
+    # Metadata
+    class Meta:
+        ordering            = ['gamer', 'total_games']
+        verbose_name        = "Stats Model"
+        verbose_name_plural = "Stats Models"
+
+    # Methods
+    def __str__(self):
+        return str(self.gamer) + " - games: " + self.total_games
