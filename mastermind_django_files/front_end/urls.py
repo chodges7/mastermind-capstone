ff8a5a0aad5367fc99b1810cd7e031589ca471a0
chodges7 <cdhodges5@gmail.com>
2022-04-26 16:39:26 -0700

    feat: add url path to stats page

--- mastermind_django_files/front_end/urls.py
+++ mastermind_django_files/front_end/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.game_view, name='game'),
+    path('stats', views.stats_view, name='stats'),
     path('about', views.about_view, name='about'),
     path('guess_entry', views.game_entry, name='entry'),
 ]
